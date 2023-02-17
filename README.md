007f06cab82ffdc0d04f7401e1a60c6f5b693d17
benixal <benixalize@gmail.com>
2023-02-17 20:00:32 +0330

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Laravel and Vue
+
+Just a simple laravel 10 and Vue 3 project
+
+## License
+
+The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const products = ref([]);
+
+onMounted( () => {
+    axios.get('api/products')
+    .then(response => products.value = response.data);
+})
+</script>
+
+<template>
+    <strong>I am a Vue App in Laravel</strong>
+    <div v-for="product in products">
+    {{  product.id  }}  : {{  product.title  }}
+    </div>
+</template>
